6b4a293d923faee4de8f368ae50ec21e61620c3a
Anji Jonnala <anjir@codeaurora.org>
2013-05-28 18:24:04 +0530

    msm8226: Update thermal script to support to link 8226 QRD config
    Based on hw_platform, differentiate 8226 normal config and QRD config.
    If hw_platform=QRD,link to QRD config otherwise link to normal config.
    
    Change-Id: Ia25e83dbf40b76c5697c6d42823a13ecd69a7f7c

--- init.qcom.thermal_conf.sh
+++ init.qcom.thermal_conf.sh
@@ -32,12 +32,16 @@
 PATH=/sbin:/system/sbin:/system/bin:/system/xbin
 export PATH
 
-platformid=`cat /sys/devices/system/soc/soc0/id`
+hw_platform=`cat /sys/devices/system/soc/soc0/hw_platform`
 
 THERMAL_ENGINE_CONF_SYMLINK=/etc/thermal-engine.conf
 # symlink already exists, exit
 if [ ! -h $THERMAL_ENGINE_CONF_SYMLINK ]; then
- case "$platformid" in
+ case "$hw_platform" in
+     "QRD") #MSM8226 QRD
+     ln -s /etc/thermal-engine-8226-qrd.conf $THERMAL_ENGINE_CONF_SYMLINK 2>/dev/null
+     ;;
+
      *) #MSM8226, etc
      ln -s /etc/thermal-engine-8226.conf $THERMAL_ENGINE_CONF_SYMLINK 2>/dev/null
      ;;
