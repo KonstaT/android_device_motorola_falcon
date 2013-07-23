0adad4b74022f6adead7923c7782b3f1d73dc959
Sooraj Sasindran <ssasin@codeaurora.org>
2013-07-23 01:11:24 -0700

    MSM8226: Enable PROTOBUF_SUPPORTED for 8226
    Enable PROTOBUF_SUPPORTED for 8226
    
    Change-Id: I943a2f2dca4948ec849b235759841e90af1afe78
    CRs-Fixed: 517338

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,5 +74,8 @@ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 # Add NON-HLOS files for ota upgrade
 ADD_RADIO_FILES ?= true
 
+# Added to indicate that protobuf-c is supported in this build
+PROTOBUF_SUPPORTED := true
+
 TARGET_USES_ION := true
 TARGET_USES_QCOM_BSP := true
