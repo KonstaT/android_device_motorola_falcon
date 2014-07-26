fddc09f9d1974b17ddcba734308dd58c82a11bcd
Konsta <konsta09@gmail.com>
2014-07-26 16:03:41 +0300

    init: update fingerprint to 4.4.4 for xt1032 and gpe

--- init/init_falcon.c
+++ init/init_falcon.c
@@ -67,16 +67,16 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         /* xt1032 GPE */
         property_set("ro.product.device", "falcon_gpe");
         property_set("ro.product.model", "Moto G");
-        property_set("ro.build.description", "falcon_gpe-user 4.4.2 KOT49H.M004 5 release-keys");
-        property_set("ro.build.fingerprint", "motorola/falcon_gpe/falcon_umts:4.4.2/KOT49H.M004/5:user/release-keys");
+        property_set("ro.build.description", "falcon_gpe-user 4.4.4 KTU84P.M003 18 release-keys");
+        property_set("ro.build.fingerprint", "motorola/falcon_gpe/falcon_umts:4.4.4/KTU84P.M003/18:user/release-keys");
         property_set("ro.mot.build.customerid", "retusa_glb");
         property_set("persist.radio.multisim.config", "");
     } else if (ISMATCH(radio, "0x1")) {
         /* xt1032 */
         property_set("ro.product.device", "falcon_umts");
         property_set("ro.product.model", "Moto G");
-        property_set("ro.build.description", "falcon_retuglb-user 4.4.3 KXB21.14-L1.32 33 release-keys");
-        property_set("ro.build.fingerprint", "motorola/falcon_retuglb/falcon_umts:4.4.3/KXB21.14-L1.32/33:user/release-keys");
+        property_set("ro.build.description", "falcon_retgb-user 4.4.4 KXB21.14-L1.40 36 release-keys");
+        property_set("ro.build.fingerprint", "motorola/falcon_retgb/falcon_umts:4.4.4/KXB21.14-L1.40/36:user/release-keys");
         property_set("ro.mot.build.customerid", "RTGB");
         property_set("persist.radio.multisim.config", "");
     } else if (ISMATCH(radio, "0x3")) {
