72bbde49e83afffd745a7e264d34f749bca3467a
Konsta <konsta09@gmail.com>
2014-10-03 18:15:08 +0300

    move symlinks to top makefile
    Change-Id: I242e0f3a94743f027454846e4ba8af1c719d1bf0

--- Android.mk
+++ Android.mk
@@ -18,4 +18,10 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),falcon)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+# Symlinks
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
+    rm $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_factory_nv.bin; \
+    ln -sf /persist/WCNSS_qcom_wlan_factory_nv.bin \
+    $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_factory_nv.bin)
 endif
