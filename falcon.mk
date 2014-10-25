12bf8dc5848a46c74d379fdcb935092c0834a68b
Konsta <konsta09@gmail.com>
2014-10-25 09:42:07 +0300

    remove separate prebuilt makefile

--- falcon.mk
+++ falcon.mk
@@ -32,4 +32,10 @@ PRODUCT_PACKAGES += \
     wcnss_service \
     pronto_wlan.ko
 
-$(call inherit-product, device/motorola/falcon/prebuilt.mk)
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/motorola/falcon/ramdisk,root)
+
+# Prebuilt
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/motorola/falcon/prebuilt/system,system)
--- prebuilt.mk
+++
@@ -1,7 +0,0 @@
-# Ramdisk
-PRODUCT_COPY_FILES := \
-    $(call find-copy-subdir-files,*,device/motorola/falcon/ramdisk,root)
-
-# Prebuilt
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,device/motorola/falcon/prebuilt/system,system)
