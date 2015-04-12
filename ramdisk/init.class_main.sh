1f8b31a4a403eba40d5cbc0c39459b27f17f57cb
Konsta <konsta09@gmail.com>
2015-04-12 17:52:05 +0300

    ramdisk: redefine rild service
    * http://review.cyanogenmod.org/#/c/91342/ allows redefining
      init.rc services so there's no need to create separate motorola
      rild service anymore.
    * Bring services inline with stock.
    
    Change-Id: I4a4d50240722df947fd9d7bb081311fafca9f717

--- ramdisk/init.class_main.sh
+++ ramdisk/init.class_main.sh
@@ -60,12 +60,12 @@ case "$baseband" in
     multisim=`getprop persist.radio.multisim.config`
 
     if [ "$multisim" = "dsds" ] || [ "$multisim" = "dsda" ]; then
-        stop mmi-ril-daemon
-        start mmi-ril-daemon
+        stop ril-daemon
+        start ril-daemon
         start ril-daemon2
     elif [ "$multisim" = "tsts" ]; then
-        stop mmi-ril-daemon
-        start mmi-ril-daemon
+        stop ril-daemon
+        start ril-daemon
         start ril-daemon2
         start ril-daemon3
     fi
